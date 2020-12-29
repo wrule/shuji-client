2dc59052b1402f638c31d27cc6500b23dfec2bb0
jimao <1982775886@qq.com>
2020-12-29 19:19:29 +0800

    定义api结构

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,35 @@
 
 export class API {
+  private project: string;
+  private method: string;
+  private pathname: string;
+  private search: string;
+  private rspBody: any;
 
+  public get Project() {
+    return this.project;
+  }
+
+  public get Method() {
+    return this.method;
+  }
+
+  public get PathName() {
+    return this.pathname;
+  }
+
+  public get Search() {
+    return this.search;
+  }
+
+  public get RspBody() {
+    return this.rspBody;
+  }
+
+  public constructor() {
+    this.project = '';
+    this.method = '';
+    this.pathname = '';
+    this.search = '';
+  }
 }
---
+++ src/api/method.ts
@@ -0,0 +1,5 @@
+
+export enum EMethod {
+  GET = 'get',
+  POST = 'post',
+}
