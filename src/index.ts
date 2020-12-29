047a4711b285fc13becf5743bea9131123860885
jimao <1982775886@qq.com>
2020-12-29 18:01:38 +0800

    客户端测试通讯

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,21 @@
 export class Client {
 
 }
+
+import * as SocketIOClient from 'socket.io-client'
+let client = SocketIOClient.io('http://localhost:9079')
+console.log('客户端已创建...');
+client.on('connect', () => {
+  console.log('已经连接到服务端');
+  let num = 0;
+  setInterval(() => {
+    console.log('发送数字到服务端 >> ', num);
+    client.emit('num', num++);
+  }, 3000);
+});
+client.on('num', (num: number) => {
+  console.log('从服务端接受到数字 << ', num);
+});
+client.on('disconnect', () => {
+  console.log('从服务端断开连接')
+});
